8f175abb078652cf3ea082f01d7f38ac0b7ad3ba
PeachPie413 <90258040+PeachPie413@users.noreply.github.com>
2022-07-17 23:47:55 -0400

    created game loop in main and worked on render manager

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,6 +1,22 @@
+from operator import truediv
 import time
 import global_variables as gb
 import render_manager
+import pygame as py
+import core_classes as core
+
 
 render_manager.create_game_window()
 
+gb.entity_world.add_processor(render_manager.Render_Processor())
+gb.entity_world.create_entity(render_manager.Renderable_Rect(py.Rect(0,0,1,1), (100,100,100)), core.Position())
+
+#main game loop
+game_done = False
+while not game_done:
+
+    for input_event in py.event.get():
+        if input_event.type == py.QUIT:
+            game_done = True
+
+    gb.entity_world.process()
--- Scripts/render_manager.py
+++ Scripts/render_manager.py
@@ -6,9 +6,11 @@ import core_classes as core
 
 
 
-
-camera_zoom = 0.0
+#width of the camera veiw in WU (world units)
+camera_zoom = 10
 camera_position = core.Position()
+#background color, when nothing is being rendered
+background_color = (255,255,255)
 
 
 
@@ -25,25 +27,45 @@ class Renderable_Rect:
     rect: py.Rect
     color: py.Color
 
-
+class Camera_Follow:
+    def __init__(self) -> None:
+        pass
 
 
 class Render_Processor(e.Processor):
 
-    def get_renderables(self):
-        pass
+    def get_world_to_pix_ratio(self):
+        return camera_zoom / gb.SCREEN_WIDTH
 
-    def scale_renderables_to_screen(self, renderables):
-        pass
+    def get_renderables(self):
+        return gb.entity_world.get_components((core.Position, Renderable_Rect))
+
+    def scale_renderables_to_screen(self, renderables, world_to_pix):
+        render_rects_scaled = []
+    
+        for ent, (render_rect, pos) in renderables:
+            scaled_rect = py.Rect(pos.x - (render_rect.width / 2.0), pos.y - (render_rect.height / 2.0), render_rect.width, render_rect.height)
+
+            render_rects_scaled.append((scaled_rect, render_rect.color))
+
+        return render_rects_scaled
 
     def draw_renderables(self, scaled_renderables):
-        pass
+
+        for (rect, color) in scaled_renderables:
+            py.draw.rect(gb.game_window, color, rect)
 
 
     def process(self):
         
+        gb.game_window.fill(background_color)
+
+        world_to_pix = self.get_world_to_pix_ratio()
+
         renderables = self.get_renderables()
 
-        scaled_renderables = self.scale_renderables_to_screen(renderables)
+        scaled_renderables = self.scale_renderables_to_screen(renderables, world_to_pix)
 
         self.draw_renderables(scaled_renderables)
+
+        py.display.update()
