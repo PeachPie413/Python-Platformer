51a023507235abec59fcbfe97110c25a72676722
PeachPie413 <90258040+PeachPie413@users.noreply.github.com>
2022-07-19 22:28:06 -0400

    write notes on input

--- Scripts/input_manager.py
+++ Scripts/input_manager.py
@@ -1,8 +1,19 @@
 import core_classes as core
 import pygame as py
 
+#for input store keys for things in global variables here, then have
+#different components for different things, ex a direction input gives
+#a vec2 for the player input dir, regardless of keys or hardware
+#things then use this dir to do stuff
 
-class Keyboard_Input():
+
+up_key = py.K_w
+down_key = py.K_s
+right_key = py.K_a
+left_key = py.K_d
+
+
+class Input_Move_Direction():
 
     def __init__(self) -> None:
         self.
