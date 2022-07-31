f2724d9fb4f66ca7fb8ad5e71c0ab6f9f6b161f2
PeachPie413 <eggcelent4@gmail.com>
2022-07-31 14:17:49 -0600

    sprite rendering

--- Scripts/render_manager.py
+++ Scripts/render_manager.py
@@ -20,13 +20,46 @@ def set_camera_zoom(zoom = 10.0):
     _camera_width = zoom
     _camera_height = zoom / gb.SCREEN_WIDTH_TO_HEIGHT_RATIO
 
+    #rescale sprites
+    scale_sprites()
+
+
 _camera_width = 10.0
 camera_zoom = _camera_width
 _camera_height = _camera_width / gb.SCREEN_WIDTH_TO_HEIGHT_RATIO
 _camera_position = core.Vector2()
 #background color, when nothing is being rendered
 background_color = (255,255,255)
+#scale for how many pixels in a sprite should be on unit long
+sprite_pix_to_world_scale = 8
+'''func for getting pixel pos of a world pos'''
+def world_to_pixel(pos = core.Vector2(), world_to_pix = 0.0):
+    screen_pos = Vector2()
+    screen_pos.x = (pos.x - _camera_position.x + _camera_width / 2.0) * world_to_pix
+    screen_pos.y = gb.SCREEN_HEIGHT - ((pos.y - _camera_position.y + _camera_height / 2.0) * world_to_pix)
+    return screen_pos
 
+#list filled with scaled sprites, in the same order as in resources
+scaled_sprites = []
+def scale_sprites():
+
+    global _camera_width
+
+    #get factor to scale sprite resolution
+    sprite_scale_factor = (gb.SCREEN_WIDTH / _camera_width) / sprite_pix_to_world_scale
+
+    #clear list of old sprites
+    scaled_sprites.clear()
+
+    #go through sprites in resources
+    for sprite in resources.sprite_list:
+
+        #scale sprite
+        ext = sprite.get_rect()[2:4]
+        scaled_sprite = py.transform.scale(sprite, (int(ext[0] * sprite_scale_factor), int(ext[1] * sprite_scale_factor)))
+
+        #add to list
+        scaled_sprites.append(scaled_sprite)
 
 
 def create_game_window():
@@ -42,12 +75,17 @@ class Renderable_Rect:
         self.height = height
 
 class Sprite:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, sprite_id = 0) -> None:
+        self.sprite_id = sprite_id
 
 class Camera_Follow:
     def __init__(self) -> None:
         pass
+
+
+
+
+
 
 
 
@@ -65,6 +103,14 @@ class Follow_Camera_Processor(e.Processor):
             _camera_position = pos.vector
 
 
+
+
+
+
+
+
+
+
 class Render_Processor(e.Processor):
 
     def get_world_to_pix_ratio(self):
@@ -77,17 +123,14 @@ class Render_Processor(e.Processor):
         global _camera_height
         global _camera_width
 
-        #flip y cause pygame does top of screen is 0,0
-        left = (pos.vector.x - _camera_position.x + _camera_width / 2.0 - unscaled_rect.width / 2.0) * world_to_pix
-        top = gb.SCREEN_HEIGHT - ((pos.vector.y - _camera_position.y + _camera_height / 2.0 + unscaled_rect.height / 2.0) * world_to_pix)
+        screen_pos = world_to_pixel(Vector2(pos.vector.x - unscaled_rect.width / 2.0, pos.vector.y + unscaled_rect.height / 2.0), world_to_pix)
 
         return py.Rect(
-            left, top,
+            screen_pos.x, screen_pos.y,
             unscaled_rect.width * world_to_pix, unscaled_rect.height * world_to_pix
         )
 
-
-    def get_scaled_renderables(self, world_to_pix):
+    def get_scaled_rects(self, world_to_pix):
         render_rects_scaled = []
     
         for ent, (pos, render_rect) in gb.entity_world.get_components(core.Position, Renderable_Rect):
@@ -95,23 +138,68 @@ class Render_Processor(e.Processor):
 
         return render_rects_scaled
 
-
-    def draw_renderables(self, scaled_renderables):
+    def draw_rects(self, scaled_renderables):
 
         for (rect, color) in scaled_renderables:
             py.draw.rect(gb.game_window, color, rect)
 
+    def render_rects(self, world_to_pix = 0.0):
+        scaled_rects = self.get_scaled_rects(world_to_pix)
+
+        self.draw_rects(scaled_rects)
+
+
+    #sprite rendering
+    def get_sprites(self):
+        sprites = []
+
+        for ent, (pos, sprite) in gb.entity_world.get_components(core.Position, Sprite):
+
+            sprites.append((pos.vector, sprite.sprite_id))
+
+        return sprites
+
+    '''take the sprites' world pos and turn it into a screen pos'''
+    def scale_sprites(self, sprites, world_to_pix):
+        
+        index = 0
+        for (pos, sprite_id) in sprites:
+
+            scaled_pos = pos * world_to_pix
+
+            sprites[index] = (scaled_pos, sprite_id)
+
+            index += 1
+
+    '''draw the scaled sprites to the screen'''
+    def draw_sprites(self, sprites):
+        
+        global scaled_sprites
+
+        for (pos, sprite_id) in sprites:
+
+            gb.game_window.blit(scaled_sprites[sprite_id], (pos.x, pos.y))
+
+    def render_sprites(self, world_to_pix):
+        sprites = self.get_sprites()
+
+        sprites = self.scale_sprites(sprites, world_to_pix)
+
+        self.draw_sprites(sprites)
 
     def process(self):
         
+        scale_sprites()
+        global scaled_sprites
+
         gb.game_window.fill(background_color)
 
         world_to_pix = self.get_world_to_pix_ratio()
 
-        scaled_renderables = self.get_scaled_renderables(world_to_pix)
+        self.render_rects(world_to_pix)
 
-        self.draw_renderables(scaled_renderables)
+        #self.render_sprites(world_to_pix)
 
-        gb.game_window.blit(resources.sprite_list[0], (50,50))
+        gb.game_window.blit(scaled_sprites[0], (50,50))
 
         py.display.update()
--- Scripts/resources.py
+++ Scripts/resources.py
@@ -4,14 +4,22 @@ import os
 
 #list of all sprite files in the game
 sprite_list = []
+#dictionary of sprite file name to id
+sprite_id_dict = {}
 
 #load all sprites in the game
 def load_sprites():
     file_list = os.listdir("Assets")
 
     #for each file in Assets
+    current_id = 0
     for file_name in file_list:
+        #add to list
         sprite_list.append(py.image.load('Assets/' + file_name))
+        #add to dict
+        sprite_id_dict[file_name] = current_id
+
+        current_id += 1
 
 
 def load_assets():
