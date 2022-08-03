b2dc03161314db15a3ec61101703c2e69ced4001
PeachPie413 <90258040+PeachPie413@users.noreply.github.com>
2022-08-03 16:42:25 -0400

    load tile types from a folder using JSON

--- Scripts/resources.py
+++ Scripts/resources.py
@@ -1,7 +1,10 @@
+from fileinput import filename
 import global_variables as gb
 from core_classes import *
 import pygame as py
 import os
+import world_data
+import json
 
 
 class Sprite_Metadata:
@@ -28,22 +31,46 @@ def load_sprites():
     for file_name in file_list:
 
         #load image
-        sprite = py.image.load('Assets/' + file_name)
+        sprite = py.image.load('Assets/Sprites/' + file_name)
 
         #add to lists
         sprite_list.append(sprite)
         sprite_metadata_list.append(Sprite_Metadata(Vector2(sprite.get_width(), sprite.get_height())))
         #add to dict
-        sprite_id_dict[file_name] = current_id
+        sprite_name, seperator, file_type = file_name.partition('.')
+        sprite_id_dict[sprite_name] = current_id
 
         current_id += 1
 
 
 #load all tile types to a dict
 def load_tile_types():
-    pass
-    #use json to save tile types
+    global tile_type_dict
+    global sprite_id_dict
+
+    file_list = os.listdir('Assets/Tile Types')
+
+    #go through all tile types
+    for file_name in file_list:
+        #open file
+        with open('Assets/Tile Types/' + file_name, 'r') as f_stream:
+
+            contents = f_stream.read()
+
+            #convert to dict
+            json_dict = json.loads(contents)
+
+            #create tile type and fil lit's data
+            tile_type = world_data.Tile_Type()
+            tile_type.sprite = sprite_id_dict[json_dict['sprite']]
+            tile_type.collides = json_dict['collides']
+            tile_type.id_name = json_dict['id_name']
+
+            #add to dict
+            tile_type_dict[json_dict['id_name']] = tile_type
+        
 
 
 def load_assets():
     load_sprites()
+    load_tile_types()
